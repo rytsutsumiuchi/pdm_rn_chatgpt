0b4b85aeaaa809d03b6055669fc215c7a9cd4f65
unknown <ricardo.ryt@gmail.com>
2023-05-19 21:25:03 -0300

    chatgpt-rn: dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 //middleware
 app.use(express.json())
 
+const { OPEN_API_KEY, PORT } = process.env
+
 app.get('/hello', (req, res) => {
     res.json({mensagem: "Hello direto do Back End"})
 })
 
-const porta = 4000
+const porta = PORT || 4000
 app.listen(porta, () => console.log (`Servidor Ok. Porta ${porta}`))
